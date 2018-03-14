9b5cb15fbdf939cfdd389aaee4bb078ceb2c306a
colorbox <colorbox222@gmail.com>
2018-03-15 00:56:49 +0900

    Fix error require insufficiency

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,9 +1,12 @@
+require 'net/http'
+require 'json'
+
 class Tweet < ApplicationRecord
   def embed_html
     url = "https://api.twitter.com/1/statuses/oembed.json?id=#{tweet_id}"
     result = Net::HTTP.get(URI.parse(url))
-    json   = JSON.parser.new(result)
-    hash   = json.parse()
+    json = JSON.parser.new(result)
+    hash = json.parse()
     hash['html']
   end
 end

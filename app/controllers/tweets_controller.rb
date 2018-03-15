d316511a89433e87c1b2c44c1392e737035c908d
colorbox <colorbox222@gmail.com>
2018-03-16 00:59:38 +0900

    Update Tweet fetch range
      取得対象となるツイートが多すぎると、タイムアウトが発生してアプリが
      動作しなくなる。
      取得対象のツイートを減らすことでその問題に対処する。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.where('created_at < ?', 1.day.ago).where('created_at > ?', 2.day.ago)
+    @tweets = Tweet.where('created_at > ?', 6.hours.ago)
   end
 end

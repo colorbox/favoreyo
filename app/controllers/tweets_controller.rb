f46e6f1e086b022f757ca7aa4b6f43e741cdf72b
colorbox <colorbox222@gmail.com>
2018-03-17 20:22:02 +0900

    Update tweet fetching rule

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    tweets = Tweet.last(50)
+    tweets = Tweet.where('created_at < ?', 1.day.ago).where('created_at > ?', 2.days.ago)
     @earlier_tweets = tweets[0..5]
     @later_tweets = tweets - @earlier_tweets
   end

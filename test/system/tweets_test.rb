3230a4e38ce44f1e1bdbcc203b70bd8ed7abc3e8
colorbox <colorbox222@gmail.com>
2018-04-01 14:40:09 +0900

    Add scroll module
      InfiniteScroll broken test
      To fix it, like scroll_to methods need
      So add that method.

--- test/system/tweets_test.rb
+++ test/system/tweets_test.rb
@@ -1,9 +1,13 @@
 require "application_system_test_case"
 
 class TweetsTest < ApplicationSystemTestCase
+  include ScrolHelper
+
   test "visiting the tweets index" do
     travel_to Time.zone.local(2018, 3, 5, 0, 00, 0)
     visit tweets_path
+    last_tweet = all('.tweet').last
+    scroll_to(last_tweet)
 
     assert_text '使ったことのないgem'
     assert_text '勤怠自動打刻マン'

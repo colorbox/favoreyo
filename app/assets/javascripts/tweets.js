a569db6c010399dabe6be21b9b943f4ada095aa4
colorbox <colorbox222@gmail.com>
2018-04-14 17:14:15 +0900

    Add infinit scroll

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,14 +1,16 @@
-// $(document).ready(function() {
-//   $( "#next" ).click(function() {
-//     $.ajax({
-//       url: "tweets.js",
-//       type: "GET",
-//       data: {
-//         last: 10,
-//       },
-//       success: function(data) {
-//         eval(data)
-//       }
-//     });
-//   });
-// });
+$(window).scroll(function(event) {
+  if ($("#next").length==0) { return }
+  var element = $("#next")
+  var hT = element.offset().top,
+      wH = $(window).height(),
+      wS = $(window).scrollTop();
+
+  // hT position get down and down with loading
+  // 1.5 times window height has two meaning
+  // 1. preload tweets
+  // 2. not to make hT position going aboid loading
+  if (hT < (wS + wH * 1.5)) {
+    element.attr("id", "fetching-next")
+    $.get("tweets.js?last=" + element.attr("last_id"))
+  }
+});

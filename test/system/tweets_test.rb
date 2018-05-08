d769561aebe5bf410375b9317f53100e731d9cb8
colorbox <colorbox222@gmail.com>
2018-05-08 20:51:59 +0900

    :lipstick:

--- test/system/tweets_test.rb
+++ test/system/tweets_test.rb
@@ -1,9 +1,9 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TweetsTest < ApplicationSystemTestCase
   include ScrolHelper
 
-  test "visiting the tweets index" do
+  test 'visiting the tweets index' do
     travel_to Time.zone.local(2018, 3, 5, 0, 00, 0)
     visit user_tweets_path(User.first.screen_name)
     last_tweet = all('.tweet').last

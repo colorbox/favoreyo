a3516dbe2b3f2f9e0ebecbff29cda3898713ff22
colorbox <colorbox222@gmail.com>
2018-05-10 23:03:04 +0900

    Simplify chrome driver setting

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,22 +1,6 @@
-require "test_helper"
-
-ActionDispatch::SystemTestCase.driver
-
-class ActionDispatch::SystemTesting::Driver # :nodoc:
-  def register
-    return unless [:selenium, :poltergeist, :webkit].include?(@name)
-
-    Capybara.register_driver @name do |app|
-      case @name
-        when :selenium then register_selenium(app)
-        when :poltergeist then register_poltergeist(app)
-        when :webkit then register_webkit(app)
-      end
-    end
-  end
-end
+require 'test_helper'
+require 'support/capybara'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  require_relative 'support/capybara'
   driven_by :headless_chrome
 end

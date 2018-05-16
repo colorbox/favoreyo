a85b9c6dbc98c1b431986a90556c60fabe02c72b
colorbox <colorbox222@gmail.com>
2018-05-16 21:33:06 +0900

    Add favorite controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :users, param: :screen_name, only: %i(index destroy) do
     resources :tweets, only: %i(index)
   end
+
+  resources :favorites, only: %i(update)
 end

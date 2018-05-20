2bd5d2af4fd7614a34b4af4a39bc137ac218d9eb
colorbox <colorbox222@gmail.com>
2018-05-20 15:03:47 +0900

    Add destroy and create favorite button on tweet index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,18 @@
 class FavoritesController < ApplicationController
   before_action :set_user
+  before_action :set_tweet
   before_action :set_client
 
   def create
-    debugger
-    tweet = Tweet.find_by(id: favorite_params)
-    @client.favorite!(tweet.tweet_id)
+    @client.favorite!(@tweet.tweet_id)
+    respond_to {|format| format.js }
+  rescue Twitter::Error::AlreadyFavorited
+    respond_to {|format| format.js }
+  end
 
+  def destroy
+    @client.unfavorite!(@tweet.tweet_id)
+    respond_to {|format| format.js }
   end
 
   private
@@ -22,5 +28,9 @@ class FavoritesController < ApplicationController
       config.access_token = @user.access_token
       config.access_token_secret = @user.access_token_secret
     end
+  end
+
+  def set_tweet
+    @tweet = Tweet.find_by(id: favorite_params)
   end
 end

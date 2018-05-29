0f5030c5b080571dc7c9fccfee6dd6d5c5bc0899
colorbox <colorbox222@gmail.com>
2018-05-29 23:54:34 +0900

    Add test for user initial tweet fetch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'webmock/minitest'
 require_relative './support/scroll_helper'
+
+WebMock.allow_net_connect!
 
 class ActiveSupport::TestCase
   fixtures :all

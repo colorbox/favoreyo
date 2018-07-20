cc68343a16e78290d40b1273d90590b860375d7b
colorbox <colorbox222@gmail.com>
2018-07-20 15:48:06 +0900

    Fix typo

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -8,7 +8,7 @@ $(window).scroll(function(event) {
   // hT position get down and down with loading
   // 1.5 times window height has two meaning
   // 1. preload tweets
-  // 2. not to make hT position going aboid loading
+  // 2. not to make hT position going avoid loading
   if (hT < (wS + wH * 1.5)) {
     element.attr("id", "fetching-next")
     $.get("tweets.js?last=" + element.attr("last_id"))

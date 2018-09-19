d5097254421eb72bc6fb24e60c57387b40092dc1
colorbox <colorbox222@gmail.com>
2018-09-19 21:02:03 +0900

    Update user index controller
      redirect to root_path when access timeline which is not published

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,9 @@ class TweetsController < ApplicationController
 
   def index
     selected_user = User.find_by(screen_name: params[:user_screen_name])
+
+    redirect_to root_path unless selected_user.timeline_published
+
     @tweets = selected_user.tweets.order(id: :desc).limit(5)
     @tweets.where!('tweets.id < ?', params[:last]) if params[:last]
 

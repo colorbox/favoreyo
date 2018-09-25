32c4a465d4009fcf5dc390c87e8818bcdb5b65fc
colorbox <colorbox222@gmail.com>
2018-09-25 23:54:55 +0900

    Owner can always look his tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   def index
     selected_user = User.find_by(screen_name: params[:user_screen_name])
 
-    redirect_to root_path unless selected_user.timeline_published
+    redirect_to root_path unless selected_user.timeline_published || @user&.id != selected_user.id
 
     @tweets = selected_user.tweets.order(id: :desc).limit(5)
     @tweets.where!('tweets.id < ?', params[:last]) if params[:last]

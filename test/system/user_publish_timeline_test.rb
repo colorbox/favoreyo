1f329c5d9a64a72d516933217b0439a0af201aab
colorbox <colorbox222@gmail.com>
2018-09-27 23:02:13 +0900

    Add user make public own timeline test

--- test/system/user_publish_timeline_test.rb
+++ test/system/user_publish_timeline_test.rb
@@ -8,4 +8,36 @@ class UserPublishTImelineTest < ApplicationSystemTestCase
 
     assert_equal(0, all('.tweet').count)
   end
+
+  test 'user publish timeline and anyone can see it' do
+    visit users_path
+
+    click_link(users(:user2).screen_name)
+
+    assert_equal(0, all('.tweet').count)
+
+    response_params = {
+      uid: users(:user2).twitter_uid,
+      info: {
+        nickname: users(:user2).screen_name
+      },
+      credentials: {
+        token: users(:user2).access_token,
+        secret: users(:user2).access_token_secret
+      }
+    }
+    OmniAuth.config.add_mock(:twitter, response_params)
+
+    visit users_path
+
+    click_link('twitter login')
+    click_link('mypage')
+    click_button('publish timeline')
+    click_link('logout')
+    visit users_path
+
+    click_link(users(:user2).screen_name)
+
+    assert_equal(5, all('.tweet').count)
+  end
 end

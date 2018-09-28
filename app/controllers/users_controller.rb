b5217c421c655fbba1a306b87608210039c9cc45
colorbox <colorbox222@gmail.com>
2018-09-28 22:49:52 +0900

    Update user index
      user index shows timeline published users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def index
-    @users = User.all
+    @users = User.where(timeline_published: true)
   end
 
   def destroy

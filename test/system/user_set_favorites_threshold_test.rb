99479ca55edec3a67a9ba6dabb74197522a06458
colorbox <colorbox222@gmail.com>
2018-11-14 23:58:54 +0900

    Add favorite_threshold update

--- test/system/user_set_favorites_threshold_test.rb
+++ test/system/user_set_favorites_threshold_test.rb
@@ -2,6 +2,7 @@ require 'application_system_test_case'
 
 class UsersSetFavoritesThresholdTest < ApplicationSystemTestCase
   test 'user can set their favorite_threshold' do
+
     visit users_path
 
     response_params = {
@@ -21,8 +22,8 @@ class UsersSetFavoritesThresholdTest < ApplicationSystemTestCase
     click_link('twitter login')
     click_link('mypage')
 
-    fill_in('favorite_threshold', with: '20')
-    click_link('save')
-    assert_equal(20, users(:user2).favorite_threshold)
+    fill_in('user_favorite_threshold', with: '20')
+    click_button('Update User')
+    assert_equal(20, users(:user2).reload.favorite_threshold)
   end
 end

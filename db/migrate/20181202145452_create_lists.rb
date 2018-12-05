43f77f853409cf0dd28805b7aff138e7cb25a7ac
colorbox <colorbox222@gmail.com>
2018-12-05 23:08:04 +0900

    Add publish column to list

--- db/migrate/20181202145452_create_lists.rb
+++ db/migrate/20181202145452_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
       t.references :user
+      t.boolean :published, default: false, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_132125) do
+ActiveRecord::Schema.define(version: 2018_12_03_145253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "list_logs", force: :cascade do |t|
+    t.bigint "list_id"
+    t.bigint "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_list_logs_on_list_id"
+    t.index ["tweet_id"], name: "index_list_logs_on_tweet_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.bigint "user_id"
+    t.boolean "published", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "timeline_logs", force: :cascade do |t|
     t.bigint "user_id"

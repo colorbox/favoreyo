1c9a32a98c5939dc1f3dd44fe50d4db684dae686
colorbox <colorbox222@gmail.com>
2018-12-27 22:18:28 +0900

    Add thresold to list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_145253) do
+ActiveRecord::Schema.define(version: 2018_12_27_125946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_145253) do
     t.boolean "published", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "lists_threshold", default: 50, null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 

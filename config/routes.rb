9d68a7dcf23308758d042084ad76a9f1b49e02ed
colorbox <colorbox222@gmail.com>
2019-02-11 00:17:27 +0900

    Restore routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
+  resource :mypage, only: %i(show)
+
+  resource :sessions, only: %i(new destroy)
+
+  resources :users, param: :screen_name, only: %i(index destroy update) do
+    resources :dates, only: %i(index)
+    resources :tweets, only: %i(index)
+  end
+
+  resources :lists, only: %i(index show create)
+  resources :list_tweets, only: %i(index)
+
+  resources :tweets, only: %i(index) do
+    resource :favorite, only: %i(create destroy)
+  end
+
+  resources :list_tweets, only: %i(index)
 end

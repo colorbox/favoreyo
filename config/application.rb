c5c942761392ff4d3f3c04e3b053a84138197146
colorbox <colorbox222@gmail.com>
2019-02-10 23:44:31 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,11 @@ Bundler.require(*Rails.groups)
 module Favoreyo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
-    config.autoload_paths += %W(#{config.root}/app/lib)
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    config.action_controller.default_protect_from_forgery = false
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
-  root 'users#index'
-
-  resource :mypage, only: %i(show)
-
-  resource :sessions, only: %i(new destroy)
-
-  resources :users, param: :screen_name, only: %i(index destroy update) do
-    resources :dates, only: %i(index)
-    resources :tweets, only: %i(index)
-  end
-
-  resources :lists, only: %i(index show create)
-  resources :list_tweets, only: %i(index)
-
-  resources :tweets, only: %i(index) do
-    resource :favorite, only: %i(create destroy)
-  end
-
-  resources :list_tweets, only: %i(index)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
